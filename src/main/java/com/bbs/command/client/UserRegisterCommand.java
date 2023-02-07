5e80bd1562c722aa0ae2b84d3d964f59ca623508
Dennis-K1 <dennis.k1.dev@gmail.com>
2023-02-07 20:21:10 +0900

    FEAT: return register form with GET request

--- src/main/java/com/bbs/command/client/UserRegisterCommand.java
+++ src/main/java/com/bbs/command/client/UserRegisterCommand.java
@@ -4,17 +4,35 @@ import com.bbs.command.Command;
 import com.bbs.command.CommandInformation;
 import com.bbs.domain.User;
 import com.bbs.service.UserService;
+import com.bbs.util.CommandUtil;
 import com.bbs.util.Role;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class UserRegisterCommand implements Command {
 
 	@Override
 	public CommandInformation execute(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+
+		if (CommandUtil.isGETMethod(request)) {
+
+			if (session.getAttribute("account")!= null) {
+				return CommandInformation.builder()
+					.isRedirect(true)
+					.path("/")
+					.build();
+			}
+
+			return CommandInformation.builder()
+				.path(CLIENT_VIEW_PATH + "registerForm.jsp")
+				.build();
+		}
 
 		String account = request.getParameter("account");
 		String password = request.getParameter("password");
@@ -27,6 +45,7 @@ public class UserRegisterCommand implements Command {
 
 		UserService userService = new UserService();
 		userService.registerUser(user);
+
 
 		return CommandInformation.builder()
 			.isRedirect(true)

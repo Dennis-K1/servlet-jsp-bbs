8ae6b4e831d76db4f1934f744f78719ebd45f9e8
Dennis-K1 <dennis.k1.dev@gmail.com>
2023-03-07 02:04:51 +0900

    FEAT: false request will return index page

--- src/main/java/com/bbs/controller/Controller.java
+++ src/main/java/com/bbs/controller/Controller.java
@@ -1,9 +1,11 @@
 package com.bbs.controller;
 
+import com.bbs.command.AdminCommands;
 import com.bbs.command.Command;
 import com.bbs.command.CommandFactory;
 import com.bbs.domain.View;
 import java.io.IOException;
+import java.util.Objects;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,9 +16,10 @@ import javax.servlet.http.HttpServletResponse;
  * 모든 요청이 시작되는 frontController
  *
  * 1. request 경로 정보를 이용하여 해당하는 Command 객체를 CommandFactory 에서 생성
- * 2. Command 객체가 요청 정보를 이용하여 로직 실행 후 해당하는 View 객체 생성 반환
- * 3. View.resolverPath 를 통해 redirect 나 forward 에 맞는 경로 resolve,
- * 4. View.render 를 통해 해당 JSP (프론트) 클라이언트 반환
+ * 2. 잘못된 요청으로 인해 Command 가 null 일 경우 인덱스 페이지 반환
+ * 3. Command 객체가 요청 정보를 이용하여 로직 실행 후 해당하는 View 객체 생성 반환
+ * 4. View.resolverPath 를 통해 redirect 나 forward 에 맞는 경로 resolve,
+ * 5. View.render 를 통해 해당 JSP (프론트) 클라이언트 반환
  */
 @WebServlet("/")
 public class Controller extends HttpServlet {
@@ -38,11 +41,22 @@ public class Controller extends HttpServlet {
 
 		Command command = CommandFactory.getCommand(requestURI);
 
+		if (!isValidRequestCommand(command)) {
+			command = CommandFactory.getCommand(AdminCommands.INDEX.getPath());
+		}
+
 		View view = command.execute(request, response);
 
 		view.resolvePath(requestURI);
 
 		view.render(request, response);
+	}
+
+	/**
+	 * 요청값이 잘못되어 CommandFactory 에서 가져온 command 가 null 일 경우 인덱스 반환
+	 */
+	private boolean isValidRequestCommand(Command command) {
+		return Objects.equals(command, null);
 	}
 
 	/**
